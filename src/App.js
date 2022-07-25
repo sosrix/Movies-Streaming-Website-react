faad7797d58d5d3d28c217e30c80b543ec56c777
SOUFIYAN AHMINA <ahminasoufiyan@gmail.com>
2022-07-25 05:55:15 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import * as ROUTES from "./pages/routes";
 import Header from "./pages/header/header";
 import Home from "./pages/home/Home";
+import Browse from "./pages/browse/browse";
+import Watch from "./pages/watch/watch";
 import Footer from "./pages/footer/Footer";
 import About from "./pages/About";
 
@@ -12,6 +14,8 @@ function App() {
       <Header />
       <Routes>
         <Route path={ROUTES.MAIN_PAGE} element={<Home />} />
+        <Route path={ROUTES.BROWSE} element={<Browse />} />
+        <Route path={ROUTES.WATCH} element={<Watch />} />
         <Route path={ROUTES.ABOUT} element={<About />} />
       </Routes>
       <Footer />
